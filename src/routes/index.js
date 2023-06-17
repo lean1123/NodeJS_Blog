e5b77d01d10f833ef8c119e28cbd06a604b60151
lean1123 <lethanhan20039@gmail.com>
2023-06-17 15:39:09 +0700

    Test husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const productsRoute = require('./products');
 const siteRoute = require('./site');
 
 function route(app) {
-    app.use('/products', productsRoute);
+    app.use("/products", productsRoute);
 
     app.use('/', siteRoute);
 }
